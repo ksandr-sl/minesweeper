8cec6718356bfd7333d6bc5db96b55fe866f02cb
ksandrsl <ksandr.sl@gmail.com>
2023-05-19 20:46:16 +0300

    refactor: createMatrix.js

--- minesweeper/js/utils/createMatrix.js
+++ minesweeper/js/utils/createMatrix.js
@@ -2,23 +2,23 @@ import Cell from "./Cell.js";
 
 export default function createMatrix(height, width) {
   const matrix = [];
-  let bombsCount = 0;
+  let number = 0;
 
   for (let i = 0; i < height; i++) {
     const row = [];
 
     for (let j = 0; j < width; j++) {
-      let random = Math.random() * 10;
-      if (bombsCount < 10 && random < 1) {
-        row.push(new Cell(true));
-        bombsCount++;
-      } else {
-        row.push(new Cell(false));
-      }
+
+      const cell = new Cell();
+      cell.number = number;
+      cell.y = i;
+      cell.x = j;
+
+      row.push(cell);
+      number++;
     }
 
     matrix.push(row);
   }
-
   return matrix;
 }
