9d6296648ddba532f8c2fa0fffe62fe6446021d8
ksandrsl <ksandr.sl@gmail.com>
2023-05-19 20:44:36 +0300

    feat: add 'checkVictory' function

---
+++ minesweeper/js/utils/checkVictory.js
@@ -0,0 +1,19 @@
+export default function checkVictory(matrix, score) {
+  let revealedCells = 0;
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+
+      if (matrix[i][j].open) {
+        revealedCells++;
+      }
+    }
+  }
+
+  if (revealedCells == 90) {
+    const victory = document.createElement('div');
+    victory.classList.add('victory');
+    victory.innerText = `Hooray! You found all mines in ${score.seconds} seconds and ${score.clicks} moves!`;
+    return victory;
+  }
+}
